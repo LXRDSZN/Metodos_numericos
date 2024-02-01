203b6fd0dc0c6785394986202271c06c412de3b5
root <root@localhost.localdomain>
2024-01-31 23:24:10 -0600

    new update

--- ProgramaunidadI.py
+++ ProgramaunidadI.py
@@ -1,3 +1,9 @@
 Valor_verdadero  = int(input("┌─[DIGITA EL VALOR VERDADERO]\n└──╼ "))
 valor_aproximado = int(input("┌─[DIGITA EL VALOR APROXIMADO]\n└──╼ "))
+resultado = ((Valor_verdadero-valor_aproximado)/Valor_verdadero)*100
+if (resultado > 0):    
+    print("Respuesta es:",resultado)
+else:
+    nuevoresultado = resultado * -1
+    print("El resultado es : ",nuevoresultado)
     
