d9e2005152423f31b5d71fd16fe7eebfbbbec7b3
root <root@LXRDSZN.LXRDSZN>
2024-02-17 11:28:20 -0600

    [17/02/24]

---
+++ Ecuaciones_no_lineales/src/ecuaciones_no_lineales/Ecuaciones_no_lineales.java
@@ -0,0 +1,18 @@
+
+package ecuaciones_no_lineales;
+
+/**
+ *
+ * @author lxrdszn
+ */
+public class Ecuaciones_no_lineales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       funcion Funcion = new funcion();
+       Funcion.funcion();
+    }
+    
+}
