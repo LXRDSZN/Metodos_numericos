028cb702b87d8cfa898b7afa9947a4aac3c04773
LXRDSZN <22680124@cuautla.tecnm.mx>
2024-04-14 12:33:29 -0600

    LXRDSZN-->Derivacion

--- U4/Integracion/src/integracion/Integracion.java
+++ U4/Integracion/src/integracion/Integracion.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package integracion;
-
+import java.util.Scanner;
 /**
  *
  * @author lxrdszn
  */
 public class Integracion {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("┌─[LXRDSZN]─[Digita el valor de X]\n└──╼ ");
+            int valor_x = sc.nextInt();
+        System.out.print("┌─[LXRDSZN]─[Digita el valor de h (0.1 por defecto)]\n└──╼ ");
+            double valor_h = sc.nextDouble();
     }
     
+}
+
+//clase funcion 
+class funcion_de_x{
+    public void funcion_de_x(){
+        
+    }
+}
+// clase para la derivacion adelante 
+class Derivacion_adelante{
+    public void Derivacion_adelante(){
+        
+    }
+}
+//clase para la derivacion centrada
+class Derivacion_centrada{
+    public void Derivacion_centrada(){
+        
+    }
+}
+//clase para la derivacion atras
+class Derivacion_atras{
+    public void Derivacion_atras(){
+        
+    }
 }
