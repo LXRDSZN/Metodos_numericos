3f57a6d23b8b9c8addbc9ae0aee8596a16bc2f13
LXRDSZN <22680124@cuautla.tecnm.mx>
2024-05-14 09:19:14 -0600

    LXRDSZN 14/05/24

--- U5/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ U5/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -8,9 +8,9 @@ public class Mavenproject1 {
         System.out.print("┌─[LXRDSZN]─[Digita la cantidad de numeros]\n└──╼ ");
         int numerotabla = sc.nextInt();
 
-        float[] tabla_x = new float[numerotabla];
-        float[] tabla_y = new float[numerotabla];
-        float suma_x = 0, suma_y = 0, media_x, media_y;
+        double[] tabla_x = new double[numerotabla];
+        double[] tabla_y = new double[numerotabla];
+        double suma_x = 0, suma_y = 0, media_x, media_y;
 
         // Datos para el arreglo de x
         for(int i = 0; i < tabla_x.length; i++){
@@ -40,18 +40,33 @@ public class Mavenproject1 {
         // Sacamos la media con respecto al valor de x e y
         media_x = suma_x / numerotabla;
         media_y = suma_y / numerotabla;
+        
+        System.out.print("\n\nMedia de x : "+ media_x +"\nmedia de y :" + media_y + "\n" );
 
         // Realizar la resta de la media con cada uno de los elementos de los arreglos x e y y mostrar el resultado
-        System.out.println("\nTabla de valores restados por la media:");
+        System.out.println("\nTabla de valores restados por la media:\n");
         System.out.println("---------------------------------");
         System.out.println("|   x - media_x |  y - media_y  |");
         System.out.println("---------------------------------");
-
+        
+        double multiplicacionacom, sumaacomulada=0,  x_exp=0, xexp_acom=0;
+        
         for(int i = 0; i < tabla_x.length; i++){
-            float resta_x = tabla_x[i]- media_x;
-            float resta_y = tabla_y[i]- media_y;
+            double resta_x = tabla_x[i]- media_x;
+            double resta_y = tabla_y[i]- media_y;
             System.out.printf("| %.5f\t| %.5f\t|\n", resta_x, resta_y);
+            
+            multiplicacionacom = resta_x * resta_y;
+            sumaacomulada += multiplicacionacom;
+            x_exp = resta_x * resta_x;
+            xexp_acom += x_exp;
         }
+       
+       double valor_b = sumaacomulada / xexp_acom;
+       double valor_a =  media_y - valor_b * media_x;
+        System.out.print("\nEl Valor de b es: "+valor_b+"\n");
+        System.out.print("El Valor de a con  a = "+ media_y + " - " +  valor_b +" * " + media_x + " es: " + valor_a+"\n");
+        System.out.print("El Valor de Y = " + valor_a+ " + " + valor_b  + " X " );
     }
 }
 
